788fba30649b834cf11ef0a0be7bfb01cd396795
Jimmy Luna <devjluna@gmail.com>
2016-05-02 10:39:12 -0500

    cambios pedido 1

--- build/js/bundle.js
+++ build/js/bundle.js
@@ -11119,19 +11119,20 @@ var animate = function() {
 
 		var t = $(window).scrollTop()
 
-		if(t > walto/2){
+		if(t > walto/2-30){
+			$('.red-content').addClass('fadeInUp')
 			$('#we').addClass('fadeInUp')
 		}
-		if(t > walto-150){
+		if(t > walto-160){
 			$('.stadium-title').addClass('fadeInUp')
 		}
-		if(t > walto){
+		if(t > walto+150){
 			$('#owl-team').addClass('fadeInUp')
 		}
-		if(t > walto+300){
+		if(t > walto+450){
 			$('.service-content').addClass('fadeInUp')
 		}
-		if(t > walto+300){
+		if(t > walto+600){
 			$('.contact-block').addClass('fadeInUp')
 			$('.animated-title').addClass('fadeInUp')
 			$('.animated-text').addClass('fadeInUp')
